de7d0f67fb420d5578f882d17c7be0dbc0086029
Anggara Setiawan <aanggaadc@gmail.com>
2022-08-06 14:16:07 +0700

    fixing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Header.css'
+import { BsPlus } from "react-icons/bs";
 
 export default function Header() {
     return (
         <header id='header'>
-            <h1>Test Case</h1>
+            <h1>Product Roadmap</h1>
+            <button>
+                <BsPlus size={20} />
+                Add New Group
+            </button>
         </header>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Home.css'
 import Header from '../../components/Header'
 
 export default function Home() {
     return (
         <div>
             <Header />
+
+            <div className='home-container'>
+
+            </div>
         </div>
     )
 }
