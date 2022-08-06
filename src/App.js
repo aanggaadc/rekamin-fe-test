bfc5f8ad78c84cd0c44a68295ab4efb3e5ba22c1
Anggara Setiawan <aanggaadc@gmail.com>
2022-08-06 13:18:06 +0700

    add route to homepage and add react-toastify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import "react-toastify/dist/ReactToastify.css"
+import {Routes, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path='/'>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+
+    <ToastContainer />
     </>
   );
 }
