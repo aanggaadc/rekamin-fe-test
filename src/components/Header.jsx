639d6e1b22f26dc753570c68bfb9044bd440d6fd
Anggara Setiawan <aanggaadc@gmail.com>
2022-08-06 13:35:49 +0700

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header id='header'>
+            <h1>Test Case</h1>
+        </header>
+    )
+}
